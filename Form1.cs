06132a9733de0a037b0ba77c84f33b6f77f19198
lyphix <lyphix@126.com>
2020-01-17 21:33:01 +0800

    代码简化，添加注释

--- Form1.cs
+++ Form1.cs
@@ -16,84 +16,51 @@ namespace Camera
         private FilterInfoCollection videoDevices;
         private VideoCaptureDevice videoDevice;
         private VideoCapabilities[] videoCapabilities;
-        private VideoCapabilities[] snapshotCapabilities;
         public Form1()
         {
             InitializeComponent();
         }
-        public bool camerastatus = false;
+        public bool camerastatus = false;//相机状态
       
        
-
-        private void buttonrandom_Click(object sender, EventArgs e)
+        
+        private void buttonrandom_Click(object sender, EventArgs e)//按下随机按钮
         {
-            int max = cmbCamera.Items.Count;
+            int max = cmbCamera.Items.Count;//取设备数
             Random num = new Random();
             int rad = num.Next(0, max);
-            cmbCamera.SelectedIndex = rad;
-            if (videoDevice != null)
-            {
-                    if ((videoCapabilities != null) && (videoCapabilities.Length != 0))
-                    {
-                        videoDevice.VideoResolution = videoCapabilities[cmbResolution.SelectedIndex];
-
-                        vispShoot.VideoSource = videoDevice;
-                        vispShoot.Start();
-                        camerastatus = true;
-                    }
-            }
+            cmbCamera.SelectedIndex = rad;//下拉菜单随机
+            reConnect();//连接相机
         }
 
-        private void Form1_Load_1(object sender, EventArgs e)
+        private void Form1_Load_1(object sender, EventArgs e)//窗体加载时
         {
             videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
-            if (videoDevices.Count != 0)
+            if (videoDevices.Count != 0)//如果电脑里有摄像头
             {
                 foreach (FilterInfo device in videoDevices)
                 {
-                    cmbCamera.Items.Add(device.Name);
+                    cmbCamera.Items.Add(device.Name);//给下拉菜单添加摄像头
                 }
             }
-            else
+            else//没有摄像头
             {
                 cmbCamera.Items.Add("没有找到摄像头");
             }
 
-            cmbCamera.SelectedIndex = 0;
+            cmbCamera.SelectedIndex = 0;//默认第一个
         }
 
-        private void cmbCamera_SelectedIndexChanged(object sender, EventArgs e)
+        private void cmbCamera_SelectedIndexChanged(object sender, EventArgs e)//摄像头菜单
         {
             if (videoDevices.Count != 0)
             {
                 videoDevice = new VideoCaptureDevice(videoDevices[cmbCamera.SelectedIndex].MonikerString);
                 GetDeviceResolution(videoDevice);
-                if (camerastatus != true)
-                {
-                    if ((videoCapabilities != null) && (videoCapabilities.Length != 0))
-                    {
-                        videoDevice.VideoResolution = videoCapabilities[cmbResolution.SelectedIndex];
-
-                        vispShoot.VideoSource = videoDevice;
-                        vispShoot.Start();
-                        camerastatus = true;
-                    }
-                }
-                else
-                {
-                    DisConnect();
-                    if ((videoCapabilities != null) && (videoCapabilities.Length != 0))
-                    {
-                        videoDevice.VideoResolution = videoCapabilities[cmbResolution.SelectedIndex];
-
-                        vispShoot.VideoSource = videoDevice;
-                        vispShoot.Start();
-                        camerastatus = true;
-                    }
-                }
+                reConnect();
             }
         }
-        private void GetDeviceResolution(VideoCaptureDevice videoCaptureDevice)
+        private void GetDeviceResolution(VideoCaptureDevice videoCaptureDevice)//分辨率函数
         {
             cmbResolution.Items.Clear();
             videoCapabilities = videoCaptureDevice.VideoCapabilities;
@@ -103,83 +70,48 @@ namespace Camera
             }
             cmbResolution.SelectedIndex = 0;
             string res = cmbResolution.SelectedItem.ToString();
-            string[] num = res.Split(new string[] { "x" }, StringSplitOptions.RemoveEmptyEntries);
-            vispShoot.Size = new System.Drawing.Size(Convert.ToInt32(num[0]), Convert.ToInt32(num[1]));
+            string[] num = res.Split(new string[] { "x" }, StringSplitOptions.RemoveEmptyEntries);//检出分辨率数值
+            vispShoot.Size = new System.Drawing.Size(Convert.ToInt32(num[0]), Convert.ToInt32(num[1]));//赋值给视频窗体
 
         }
 
-        //private void buttonopen_Click(object sender, EventArgs e)
-        //{
-        //    if (videoDevice != null)
-        //    {
-        //        if (camerastatus != true)
-        //        {
-        //            if ((videoCapabilities != null) && (videoCapabilities.Length != 0))
-        //            {
-        //                videoDevice.VideoResolution = videoCapabilities[cmbResolution.SelectedIndex];
-
-        //                vispShoot.VideoSource = videoDevice;
-        //                vispShoot.Start();
-        //                camerastatus = true;
-        //            }
-        //        }
-        //        else
-        //        {
-        //            DisConnect();
-        //            if ((videoCapabilities != null) && (videoCapabilities.Length != 0))
-        //            {
-        //                videoDevice.VideoResolution = videoCapabilities[cmbResolution.SelectedIndex];
-
-        //                vispShoot.VideoSource = videoDevice;
-        //                vispShoot.Start();
-        //                camerastatus = true;
-        //            }
-        //        }
-        //    }
-        //}
-
-        private void cmbResolution_SelectedIndexChanged(object sender, EventArgs e)
+        private void cmbResolution_SelectedIndexChanged(object sender, EventArgs e)//分辨率菜单
         {
             string res = cmbResolution.SelectedItem.ToString();
             string[] num = res.Split(new string[] { "x" }, StringSplitOptions.RemoveEmptyEntries);
             vispShoot.Size = new System.Drawing.Size(Convert.ToInt32(num[0]), Convert.ToInt32(num[1]));
-            if (camerastatus != true)
-            {
-                if ((videoCapabilities != null) && (videoCapabilities.Length != 0))
-                {
-                    videoDevice.VideoResolution = videoCapabilities[cmbResolution.SelectedIndex];
-
-                    vispShoot.VideoSource = videoDevice;
-                    vispShoot.Start();
-                    camerastatus = true;
-                }
-            }
-            else
-            {
-                DisConnect();
-                if ((videoCapabilities != null) && (videoCapabilities.Length != 0))
-                {
-                    videoDevice.VideoResolution = videoCapabilities[cmbResolution.SelectedIndex];
-
-                    vispShoot.VideoSource = videoDevice;
-                    vispShoot.Start();
-                    camerastatus = true;
-                }
-            }
-        }
-        private void DisConnect()
-        {
-            if (vispShoot.VideoSource != null)
-            {
-                vispShoot.SignalToStop();
-                vispShoot.WaitForStop();
-                vispShoot.VideoSource = null;
-            }
-        }
-
-        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+            reConnect();
+        }       
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)//窗体关闭
         {
             DisConnect();
         }
+
+        private void DisConnect()//断开相机
+        {
+            if (camerastatus == true)
+            {
+                vispShoot.SignalToStop();
+                vispShoot.WaitForStop();
+                camerastatus = false;
+            }
+        }
+        private void Connect()//连接相机
+        {
+            if ((videoCapabilities != null) && (videoCapabilities.Length != 0))
+            {
+                videoDevice.VideoResolution = videoCapabilities[cmbResolution.SelectedIndex];
+                vispShoot.VideoSource = videoDevice;
+                vispShoot.Start();
+                camerastatus = true;
+            }
+        }
+        private void reConnect()//重连接相机
+        {
+                DisConnect();
+                Connect();
+        }
+
+
     }
 }
