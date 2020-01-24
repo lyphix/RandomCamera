ab24951567dc7ddafa2a531ce4d60e31a4311b8e
lyphix <lyphix@126.com>
2020-01-24 11:44:04 +0800

    v2.0 按照需求更新程序

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,99 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.cmbResolution = new System.Windows.Forms.ComboBox();
             this.vispShoot = new AForge.Controls.VideoSourcePlayer();
             this.buttonrandom = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
             this.cmbCamera = new System.Windows.Forms.ComboBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.cbFullScreen = new System.Windows.Forms.CheckBox();
+            this.tbTimer = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 12);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "分辨率：";
             // 
             // cmbResolution
             // 
             this.cmbResolution.FormattingEnabled = true;
-            this.cmbResolution.Location = new System.Drawing.Point(72, 10);
+            this.cmbResolution.Location = new System.Drawing.Point(12, 12);
             this.cmbResolution.Name = "cmbResolution";
             this.cmbResolution.Size = new System.Drawing.Size(121, 20);
             this.cmbResolution.TabIndex = 1;
-            this.cmbResolution.SelectedIndexChanged += new System.EventHandler(this.cmbResolution_SelectedIndexChanged);
             // 
             // vispShoot
             // 
-            this.vispShoot.Location = new System.Drawing.Point(12, 36);
+            this.vispShoot.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.vispShoot.Location = new System.Drawing.Point(12, 12);
             this.vispShoot.Name = "vispShoot";
-            this.vispShoot.Size = new System.Drawing.Size(629, 344);
+            this.vispShoot.Size = new System.Drawing.Size(897, 513);
             this.vispShoot.TabIndex = 2;
             this.vispShoot.Text = "videoSourcePlayer1";
             this.vispShoot.VideoSource = null;
             // 
             // buttonrandom
             // 
-            this.buttonrandom.Location = new System.Drawing.Point(385, 7);
+            this.buttonrandom.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonrandom.Location = new System.Drawing.Point(406, 272);
             this.buttonrandom.Name = "buttonrandom";
             this.buttonrandom.Size = new System.Drawing.Size(108, 23);
             this.buttonrandom.TabIndex = 3;
-            this.buttonrandom.Text = "打开随机摄像头";
+            this.buttonrandom.Text = "开始";
             this.buttonrandom.UseVisualStyleBackColor = true;
             this.buttonrandom.Click += new System.EventHandler(this.buttonrandom_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(199, 13);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 12);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "摄像头：";
             // 
             // cmbCamera
             // 
             this.cmbCamera.FormattingEnabled = true;
-            this.cmbCamera.Location = new System.Drawing.Point(258, 9);
+            this.cmbCamera.Location = new System.Drawing.Point(139, 12);
             this.cmbCamera.Name = "cmbCamera";
             this.cmbCamera.Size = new System.Drawing.Size(121, 20);
             this.cmbCamera.TabIndex = 5;
             this.cmbCamera.SelectedIndexChanged += new System.EventHandler(this.cmbCamera_SelectedIndexChanged);
             // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(12, 531);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(897, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // cbFullScreen
+            // 
+            this.cbFullScreen.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cbFullScreen.AutoSize = true;
+            this.cbFullScreen.Location = new System.Drawing.Point(535, 301);
+            this.cbFullScreen.Name = "cbFullScreen";
+            this.cbFullScreen.Size = new System.Drawing.Size(84, 16);
+            this.cbFullScreen.TabIndex = 7;
+            this.cbFullScreen.Text = "是否全屏化";
+            this.cbFullScreen.UseVisualStyleBackColor = true;
+            // 
+            // tbTimer
+            // 
+            this.tbTimer.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tbTimer.Location = new System.Drawing.Point(604, 274);
+            this.tbTimer.Name = "tbTimer";
+            this.tbTimer.Size = new System.Drawing.Size(39, 21);
+            this.tbTimer.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(533, 277);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "倒计时(秒)";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSize = true;
-            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.cmbCamera);
-            this.Controls.Add(this.label2);
+            this.ClientSize = new System.Drawing.Size(921, 566);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbTimer);
+            this.Controls.Add(this.cbFullScreen);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.buttonrandom);
             this.Controls.Add(this.vispShoot);
+            this.Controls.Add(this.cmbCamera);
             this.Controls.Add(this.cmbResolution);
-            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
             this.Name = "Form1";
-            this.Text = "随机相机";
+            this.Text = "随机摄像头";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox cmbResolution;
         private AForge.Controls.VideoSourcePlayer vispShoot;
         private System.Windows.Forms.Button buttonrandom;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox cmbCamera;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.CheckBox cbFullScreen;
+        private System.Windows.Forms.TextBox tbTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -21,16 +21,21 @@ namespace Camera
             InitializeComponent();
         }
         public bool camerastatus = false;//相机状态
-      
-       
-        
-        private void buttonrandom_Click(object sender, EventArgs e)//按下随机按钮
+        int count=0;
+        int time =1200;//毫秒
+        int CameraNum = 0;
+        int[] CameraArray = new int[10];
+        Random rd = new Random();
+
+
+        private void buttonrandom_Click(object sender, EventArgs e)//按下开始按钮
         {
-            int max = cmbCamera.Items.Count;//取设备数
-            Random num = new Random();
-            int rad = num.Next(0, max);
-            cmbCamera.SelectedIndex = rad;//下拉菜单随机
-            reConnect();//连接相机
+            if (cbFullScreen.Checked)//检查全屏模式
+            {
+                this.FormBorderStyle = FormBorderStyle.None;     //设置窗体为无边框样式
+                this.WindowState = FormWindowState.Maximized;    //最大化窗体 
+            }
+            GameBegin();//游戏开始
         }
 
         private void Form1_Load_1(object sender, EventArgs e)//窗体加载时
@@ -45,10 +50,11 @@ namespace Camera
             }
             else//没有摄像头
             {
-                cmbCamera.Items.Add("没有找到摄像头");
+                MessageBox.Show("没有找到摄像头", "错误");
             }
-
+            tbTimer.Text = (time/10).ToString();//给tb默认值2分钟
             cmbCamera.SelectedIndex = 0;//默认第一个
+            CameraNum = cmbCamera.Items.Count;
         }
 
         private void cmbCamera_SelectedIndexChanged(object sender, EventArgs e)//摄像头菜单
@@ -57,31 +63,33 @@ namespace Camera
             {
                 videoDevice = new VideoCaptureDevice(videoDevices[cmbCamera.SelectedIndex].MonikerString);
                 GetDeviceResolution(videoDevice);
-                reConnect();
             }
         }
         private void GetDeviceResolution(VideoCaptureDevice videoCaptureDevice)//分辨率函数
         {
+            int MaxResid = 0;
+            int Resid = 0;
+            int Res1 = 0;
             cmbResolution.Items.Clear();
             videoCapabilities = videoCaptureDevice.VideoCapabilities;
             foreach (VideoCapabilities capabilty in videoCapabilities)
             {
                 cmbResolution.Items.Add($"{capabilty.FrameSize.Width} x {capabilty.FrameSize.Height}");
+                int Res2 = capabilty.FrameSize.Width * capabilty.FrameSize.Height;
+                if (Res2 > Res1)//计算摄像头支持的最大分辨率
+                {
+                    MaxResid = Resid;
+                }
+                Res1 = Res2;
+                Resid++;
             }
-            cmbResolution.SelectedIndex = 0;
+            cmbResolution.SelectedIndex = MaxResid;
             string res = cmbResolution.SelectedItem.ToString();
             string[] num = res.Split(new string[] { "x" }, StringSplitOptions.RemoveEmptyEntries);//检出分辨率数值
-            vispShoot.Size = new System.Drawing.Size(Convert.ToInt32(num[0]), Convert.ToInt32(num[1]));//赋值给视频窗体
+            //vispShoot.Size = new System.Drawing.Size(Convert.ToInt32(num[0]), Convert.ToInt32(num[1]));//赋值给视频窗体
 
         }
-
-        private void cmbResolution_SelectedIndexChanged(object sender, EventArgs e)//分辨率菜单
-        {
-            string res = cmbResolution.SelectedItem.ToString();
-            string[] num = res.Split(new string[] { "x" }, StringSplitOptions.RemoveEmptyEntries);
-            vispShoot.Size = new System.Drawing.Size(Convert.ToInt32(num[0]), Convert.ToInt32(num[1]));
-            reConnect();
-        }       
+        
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)//窗体关闭
         {
             DisConnect();
@@ -112,6 +120,96 @@ namespace Camera
                 Connect();
         }
 
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)//按键时间
+        {
+            if (e.KeyChar == (char)Keys.Escape)
+            {
+                this.FormBorderStyle = FormBorderStyle.FixedDialog;
+                ResetSetting();
+            }
+            if(e.KeyChar == (char)Keys.Space)
+            {
+                if (this.buttonrandom.Visible == false)
+                {
+                    NextCamera();
+                }
+                else
+                {
+                    MessageBox.Show("请先按开始", "");
+                }
+                
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            count++;
+            progressBar1.Value = count;//设置进度条进度
+            if (count == time)
+            {
+                timer1.Stop();//停止计时
+                progressBar1.Value = 0;
+                count = 0;
+                MessageBox.Show("You Lose", "");
+                ResetCamera();
+            }
+        }
+
+        private void GameBegin()
+        {
+            this.buttonrandom.Visible = false;
+            this.label1.Visible = false;
+            this.cbFullScreen.Visible = false;
+            this.tbTimer.Visible = false;
+
+
+
+            CameraArray = Enumerable.Range(0,cmbCamera.Items.Count)
+                        .OrderBy(n => (rd.Next()))
+                        .ToArray<int>();
+            cmbCamera.SelectedIndex = CameraArray[CameraNum-1];
+            reConnect();
+            time = Convert.ToInt32(tbTimer.Text) * 10;
+            progressBar1.Maximum = time;
+            timer1.Start();
+        }
+
+        private void NextCamera()
+        {
+            CameraNum--;
+            if (CameraNum != 0)
+            {
+                cmbCamera.SelectedIndex = CameraArray[CameraNum-1];
+                reConnect();
+                timer1.Stop();//停止计时
+                progressBar1.Value = 0;
+                count = 0;
+                timer1.Start();
+            }
+            else
+            {
+                MessageBox.Show("You Win", "");
+                ResetCamera();
+            }
+        }
+
+        private void ResetCamera()
+        {
+            timer1.Stop();//停止计时
+            progressBar1.Value = 0;
+            count = 0;
+            DisConnect();
+            this.buttonrandom.Visible = true;
+            CameraNum = cmbCamera.Items.Count;
+        }
+
+        private void ResetSetting()
+        {
+            ResetCamera();
+            this.label1.Visible = true;
+            this.cbFullScreen.Visible = true;
+            this.tbTimer.Visible = true;
+        }
 
     }
 }
